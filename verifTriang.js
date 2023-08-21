7a894058c89ab88f376e721be7ff07eea91c7ea6
Luiz Fernando de Albuquerque Gomes <20231370027@ad.ifpb.edu.br>
2023-08-21 16:10:36 -0300

    create verifTriang

---
+++ verifTriang.js
@@ -0,0 +1,16 @@
+function verificaTriang(...lados){
+    if(lados.length>3 || lados.length<3){
+        return "Não é um triângulo"
+    }
+    if (lados[0] === lados[1] && lados[1] === lados[2] && lados[2]===lados[0]){
+        return "Equilátero"
+    }
+    if (lados[0] !== lados[1] && lados[1] !== lados[2] && lados[2] !== lados[0]){
+        return "Escaleno"
+    }
+    else{
+        return "Isósceles"
+    }
+}
+
+console.log(verificaTriang(2,4,2))
