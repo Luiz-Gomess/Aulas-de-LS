00b91f4c7959235ce9fcf053a3e1e77770cd2e5a
LuizFernando <128930132+LuizFernando12@users.noreply.github.com>
2023-08-23 15:00:54 -0300

    Create AULA03.js

---
+++ exercicios/AULA03.js
@@ -0,0 +1,65 @@
+// const operando1 = 30 
+// const operando2 = 5
+
+// let operador = 'c'
+// let res = undefined
+
+// ESTRUTURAS DE DECISÃO
+
+// if(operador === '+'){
+//     res = operando1 + operando2;
+// }else if(operador === '-'){
+//     res = operando1 - operando2
+// }else if(operador === '*'){
+//     res = operando1 * operando2
+// }else if(operador === "/"){
+//     res = operando1 / operando2
+// }else if(operador === "%"){
+//     res = operando1%operando2
+// }
+
+// console.log(`O res da ${operador} é ${res}`)
+
+// switch(operador){
+//     case '+': res = operando1 + operando2;break;
+//     case '-': res = operando1 - operando2;break;
+//     case '*': res = operando1 * operando2;break;
+//     case '/': res = operando1 / operando2;break;
+//     case '%': res = operando1 % operando2;break;
+//     default : res = 'Operação não suportada!'
+// }
+
+// console.log(res)
+
+// resultado = operando2 + operando1 === 30? "SIM": "NÃO"
+// console.log(resultado)
+
+
+
+//ESTRUTURA DE REPETIÇÃO
+
+//FOR
+
+// for( let i = 0; i <10; i++){
+//     console.log('Valor: ' + (i+1));
+// }
+
+//WHILE
+
+// i = 0
+// while(i<10){
+//     console.log('Valor: '+i);
+//     i++
+// }
+
+// ePrimo = true
+
+// for(let i = 2; i<51;i++){
+//     for(let j=1; i;j++){
+//         if ((i%j)===0){
+//             ePrimo = false
+//             break
+//         }
+//     }
+//     console.log(`${i} é Primo.`)
+// }
